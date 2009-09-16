b85412afc851865b7f32eb617e210f6203d04ffe
Diego Carrion <dc.rec1@gmail.com>
2009-09-16 19:31:04 -0300

    build fetches and builds ther repository on creation

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,4 +1,13 @@
 class Build < ActiveRecord::Base
   belongs_to :project
   validates_presence_of :project, :log, :success
+
+  def before_validation_on_create
+    unless project.nil?
+      Kernel.system "cd #{project.path} && git fetch origin master > tmp/log"
+      Kernel.system "cd #{@project.path} && rake build >> tmp/log"
+      self.log = File.open("#{project.path}/tmp/log").read
+      self.success = true
+    end
+  end
 end
--- spec/models/build_spec.rb
+++ spec/models/build_spec.rb
@@ -3,4 +3,33 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Build do
   should_belong_to :project
   should_validate_presence_of :project, :log, :success
+
+  context "on creation" do
+    before :each do
+      Kernel.stub!(:system).and_return(false)
+      File.stub!(:open).and_return(mock(Object, :read => "lorem ipsum"))
+      @project = Project.koujou
+    end
+
+    it "should fetch the repository" do
+      Kernel.should_receive(:system).with("cd #{@project.path} && git fetch origin master > tmp/log")
+      Build.create! :project => @project
+    end
+
+    it "should build the project" do
+      Kernel.should_receive(:system).with("cd #{@project.path} && rake build >> tmp/log")
+      Build.create! :project => @project
+    end
+
+    it "should save the log" do
+      log = "Can't touch this!"
+      File.stub!(:open).with("#{@project.path}/tmp/log").and_return(mock(Object, :read => log))
+      Build.create!(:project => @project).log.should eql(log)
+    end
+
+    it "should determine if the build was a success or not" do
+      Kernel.stub!(:system).and_return(false)
+      Build.create!(:project => @project).success.should be_false
+    end
+  end
 end
