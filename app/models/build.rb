954f82ac29574d644c336f7ba3b3fd1126cdc699
Diego Carrion <dc.rec1@gmail.com>
2009-09-16 19:53:35 -0300

    a build is saved specifying if was successful or not

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,13 +1,18 @@
 class Build < ActiveRecord::Base
   belongs_to :project
-  validates_presence_of :project, :log, :success
+  validates_presence_of :project, :log
 
   def before_validation_on_create
     unless project.nil?
-      Kernel.system "cd #{project.path} && git fetch origin master > tmp/log"
-      Kernel.system "cd #{@project.path} && rake build >> tmp/log"
-      self.log = File.open("#{project.path}/tmp/log").read
-      self.success = true
+      Kernel.system "cd #{project.path} && git fetch origin master > #{log_path}"
+      self.successful = Kernel.system "cd #{@project.path} && rake build >> #{log_path}"
+      self.log = File.open(log_path).read
     end
+  end
+
+  private
+
+  def log_path
+    "#{RAILS_ROOT}/tmp/#{project.name}"
   end
 end
--- spec/models/build_spec.rb
+++ spec/models/build_spec.rb
@@ -2,34 +2,35 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Build do
   should_belong_to :project
-  should_validate_presence_of :project, :log, :success
+  should_validate_presence_of :project, :log
 
   context "on creation" do
     before :each do
       Kernel.stub!(:system).and_return(false)
       File.stub!(:open).and_return(mock(Object, :read => "lorem ipsum"))
       @project = Project.koujou
+      @log_path = "#{RAILS_ROOT}/tmp/#{@project.name}"
     end
 
     it "should fetch the repository" do
-      Kernel.should_receive(:system).with("cd #{@project.path} && git fetch origin master > tmp/log")
+      Kernel.should_receive(:system).with("cd #{@project.path} && git fetch origin master > #{@log_path}")
       Build.create! :project => @project
     end
 
     it "should build the project" do
-      Kernel.should_receive(:system).with("cd #{@project.path} && rake build >> tmp/log")
+      Kernel.should_receive(:system).with("cd #{@project.path} && rake build >> #{@log_path}")
       Build.create! :project => @project
     end
 
     it "should save the log" do
       log = "Can't touch this!"
-      File.stub!(:open).with("#{@project.path}/tmp/log").and_return(mock(Object, :read => log))
+      File.stub!(:open).with(@log_path).and_return(mock(Object, :read => log))
       Build.create!(:project => @project).log.should eql(log)
     end
 
     it "should determine if the build was a success or not" do
       Kernel.stub!(:system).and_return(false)
-      Build.create!(:project => @project).success.should be_false
+      Build.create!(:project => @project).successful.should be_false
     end
   end
 end
