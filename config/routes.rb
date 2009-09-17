8d0104b9d7059f93035b647d7d6900215fb1dc07
Diego Carrion <dc.rec1@gmail.com>
2009-09-16 21:45:29 -0300

    projects controller is the root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :builds
-
   map.resources :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -35,7 +34,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "projects"
 
   # See how all your routes lay out with "rake routes"
 
