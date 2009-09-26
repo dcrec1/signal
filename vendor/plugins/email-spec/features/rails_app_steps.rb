71ff0130c9628ed5e50ee5054c4cce977542f8a1
Diego Carrion <dc.rec1@gmail.com>
2009-09-26 12:18:23 -0300

    email-spec is a plugin

---
+++ vendor/plugins/email-spec/features/rails_app_steps.rb
@@ -0,0 +1,28 @@
+require 'fileutils'
+
+Given /^the example rails app is setup with the latest generators$/ do
+  root_dir = File.join(File.expand_path(File.dirname(__FILE__)), "..")
+
+  email_specs_path = "#{root_dir}/examples/rails_root/features/step_definitions/email_steps.rb"
+  FileUtils.rm(email_specs_path) if File.exists?(email_specs_path)
+  FileUtils.mkdir_p("#{root_dir}/examples/rails_root/vendor/plugins/email_spec")
+  FileUtils.cp_r("#{root_dir}/rails_generators", "#{root_dir}/examples/rails_root/vendor/plugins/email_spec/")
+
+  Dir.chdir(File.join(root_dir, 'examples', 'rails_root')) do
+    system "./script/generate email_spec"
+  end
+
+end
+
+When /^I run "([^\"]*)"$/ do |cmd|
+  cmd.gsub!('cucumber', "#{Cucumber::RUBY_BINARY} #{Cucumber::BINARY}")
+  root_dir = File.join(File.expand_path(File.dirname(__FILE__)), "..")
+  Dir.chdir(File.join(root_dir, 'examples', 'rails_root')) do
+    @output = `#{cmd}`
+  end
+end
+
+Then /^I should see the following summary report:$/ do |expected_report|
+  @output.should include(expected_report)
+end
+
