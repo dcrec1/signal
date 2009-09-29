386f4467ccfbb339e2f26dc92329066ed08fb3a0
Diego Carrion <dc.rec1@gmail.com>
2009-09-29 13:12:43 -0300

    added a link to features in projects page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
     project.connect 'build', :controller => 'projects', :action => 'build'
   end
   map.root :controller => "projects"
-  map.metrics "/projects/:name/tmp/metric_fu/output/index.html", :controller => nil
+  map.metrics  "/projects/:name/tmp/metric_fu/output/index.html", :controller => nil
   map.specs    "/projects/:name/doc/specs.html",                  :controller => nil
+  map.features "/projects/:name/doc/features.html",               :controller => nil
 end
