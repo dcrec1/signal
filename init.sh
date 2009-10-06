f33e0a16039ce8245c003bed1c86942a9e15c4c4
Diego Carrion <dc.rec1@gmail.com>
2009-10-06 15:27:50 -0300

    removed init and update because inploy does the same

--- init.sh
+++
@@ -1,4 +0,0 @@
-cp config/mailer.yml.sample config/mailer.yml
-cp config/database.yml.sample config/database.yml
-sudo rake gems:install
-mkdir -p tmp/pids
--- update.sh
+++
@@ -1,4 +0,0 @@
-git pull origin master
-sudo rake gems:install
-rake db:migrate RAILS_ENV=production
-rake asset:packager:build_all
