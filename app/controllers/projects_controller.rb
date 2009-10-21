4c21572ab7d4f27dc8a91a9443a9b2114069b882
Diego Carrion <dc.rec1@gmail.com>
2009-10-20 22:39:27 -0200

    projects.show is cached

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,11 @@
 class ProjectsController < InheritedResources::Base
 
-  caches_page :index
+  caches_page :index, :show
 
   def build
     Project.find(params[:project_id]).send_later :build
     expire_page :action => :index
+    expire_page :action => :show
     render :nothing => true
   end
 end
