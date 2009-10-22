afab52c0507b1882dd8546c952080cc6128a7ae6
Diego Carrion <dc.rec1@gmail.com>
2009-10-22 21:18:05 -0200

    a deploy should be saved with a project, an output and a success boolean

--- app/models/deploy.rb
+++ app/models/deploy.rb
@@ -1,3 +1,4 @@
 class Deploy < ActiveRecord::Base
   belongs_to :project
+  validates_presence_of :project, :output, :success
 end
--- spec/models/deploy_spec.rb
+++ spec/models/deploy_spec.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Deploy do
-
+  should_validate_presence_of :project, :output, :success
 end
