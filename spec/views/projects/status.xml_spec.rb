2f826369ba05993f9be67e12c9473e48f6b9db23
Diego Carrion <dc.rec1@gmail.com>
2010-04-05 14:46:13 -0300

    show the author of the commit in the new email format

--- spec/views/projects/status.xml_spec.rb
+++ spec/views/projects/status.xml_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/projects/status.xml.haml" do
   context "rendering a xml document respecting CCMMenu specifications" do
