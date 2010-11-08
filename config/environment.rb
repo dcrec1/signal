37228cf551d175e1fd4b37d9bd5a446319c664ca
Stefan Botzenhart <sb@basiszwo.com>
2010-11-09 04:38:09 +0800

    change mailer config to provide a gmail example; load all available mailer settings;

--- config/environment.rb
+++ config/environment.rb
@@ -6,17 +6,11 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
-MAILER = YAML.load_file("#{RAILS_ROOT}/config/mailer.yml")
-
 Rails::Initializer.run do |config|
   
   config.time_zone = 'UTC'
 
   config.action_controller.page_cache_directory = RAILS_ROOT + "/public/cache"
 
-  config.action_mailer.smtp_settings = {
-    :address          => MAILER['address'],
-    :port             => MAILER['port'],
-    :domain           => MAILER['domain']
-  }
+  config.action_mailer.smtp_settings = YAML.load_file("#{RAILS_ROOT}/config/mailer.yml")
 end
