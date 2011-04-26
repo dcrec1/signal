ba3a27908487ec6066756aad5649734f2e331583
DeLynn Berry <delynn@gmail.com>
2011-04-26 07:58:42 -0600

    Only parse the mailer config file if it exists.

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,17 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module SignalCI
   class Application < Rails::Application
-    MAILER = YAML.load_file Rails.root.join("config/mailer.yml")
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
     config.action_controller.page_cache_directory = Rails.root.join("public/cache")
-    config.action_mailer.smtp_settings = {
-      :address          => MAILER['address'],
-      :port             => MAILER['port'],
-      :domain           => MAILER['domain'],
-      :enable_starttls_auto => false
-    }
+    if Rails.root.join("config/mailer.yml").file?
+      MAILER = YAML.load_file(Rails.root.join("config/mailer.yml"))
+      config.action_mailer.smtp_settings = {
+        :address          => MAILER['address'],
+        :port             => MAILER['port'],
+        :domain           => MAILER['domain'],
+        :enable_starttls_auto => false
+      }
+    end
   end
 end
